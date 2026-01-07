26c63c5d55e2d4ffdeda691f4ffd628f08ed2210
TANIGUCHI Masaya <ta2gch@gmail.com>
2026-01-07 12:28:40 +0400

    fix: import markup-templating for Prism handlebars support

--- src/components/MonospaceEditor.vue
+++ src/components/MonospaceEditor.vue
@@ -15,6 +15,7 @@ import "prismjs/components/prism-css";
 import "prismjs/components/prism-scss";
 import "prismjs/components/prism-less";
 import "prismjs/components/prism-markup";
+import "prismjs/components/prism-markup-templating";
 import "prismjs/components/prism-yaml";
 import "prismjs/components/prism-markdown";
 import "prismjs/components/prism-graphql";
